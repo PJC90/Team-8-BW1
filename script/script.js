fcff1fa51ef6db3866bdf4d2dc9a9627ba3b90a3
FedericoCaglayan <143503129+FedericoCaglayan@users.noreply.github.com>
2023-09-19 11:57:43 +0200

    commit with graphic(non completo)

--- script/script.js
+++ script/script.js
@@ -1,2 +1,26 @@
+   
+    var data = {
+        labels: ['Risposte Giuste', 'Risposte Sbagliate'],
+        datasets: [{
+            data: [7, 3], 
+            backgroundColor: ['green', 'red']
+        }]
+    };
 
+    var ctx = document.getElementById('myDonutChart').getContext('2d');
+
+    var myDonutChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: data,
+        options: {
+            responsive: true,
+            legend: {
+                position: 'bottom',
+            },
+            title: {
+                display: true,
+                text: 'Percentuale di Risposte Giuste su 10 Domande',
+            },
+        },
+    });
 
