f613eebc07d908716297bf8d08ed7379f7c0e373
Carmen Caniglia <carmencaniglia@gmail.com>
2023-09-20 10:04:48 +0200

    input button

--- script/main.js
+++ script/main.js
@@ -1,8 +1,5 @@
-const proceed = function (e) {
+const form = document.getElementById("checkbox");
+form.addEventListener("submit", function (e) {
   e.preventDefault();
-};
-
-const button = document.getElementsByClassName("button");
-button.addeventlistener("click", function () {
-  window.location.href("quiz.html");
+  window.open("quiz.html", "_self");
 });
