5ff5d8e7250ad3b254324ea4e6550901b26dcaff
Pierpaolo Colasante <colasantepierpaolo@gmail.com>
2023-09-21 12:07:56 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,195 +1,195 @@
 const questions = [
   {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
-    question: "What does CPU stand for?",
-    correct_answer: "Central Processing Unit",
+    category: 'Science: Computers',
+    type: 'multiple',
+    difficulty: 'easy',
+    question: 'What does CPU stand for?',
+    correct_answer: 'Central Processing Unit',
     incorrect_answers: [
-      "Central Process Unit",
-      "Computer Personal Unit",
-      "Central Processor Unit",
+      'Central Process Unit',
+      'Computer Personal Unit',
+      'Central Processor Unit',
     ],
   },
   {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
+    category: 'Science: Computers',
+    type: 'multiple',
+    difficulty: 'easy',
     question:
       "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn't get modified?",
-    correct_answer: "Final",
-    incorrect_answers: ["Static", "Private", "Public"],
+    correct_answer: 'Final',
+    incorrect_answers: ['Static', 'Private', 'Public'],
   },
   {
-    category: "Science: Computers",
-    type: "boolean",
-    difficulty: "easy",
-    question: "The logo for Snapchat is a Bell.",
-    correct_answer: "False",
-    incorrect_answers: ["True"],
+    category: 'Science: Computers',
+    type: 'boolean',
+    difficulty: 'easy',
+    question: 'The logo for Snapchat is a Bell.',
+    correct_answer: 'False',
+    incorrect_answers: ['True'],
   },
   {
-    category: "Science: Computers",
-    type: "boolean",
-    difficulty: "easy",
+    category: 'Science: Computers',
+    type: 'boolean',
+    difficulty: 'easy',
     question:
-      "Pointers were not used in the original C programming language; they were added later on in C++.",
-    correct_answer: "False",
-    incorrect_answers: ["True"],
+      'Pointers were not used in the original C programming language; they were added later on in C++.',
+    correct_answer: 'False',
+    incorrect_answers: ['True'],
   },
   {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
+    category: 'Science: Computers',
+    type: 'multiple',
+    difficulty: 'easy',
     question:
-      "What is the most preferred image format used for logos in the Wikimedia database?",
-    correct_answer: ".svg",
-    incorrect_answers: [".png", ".jpeg", ".gif"],
+      'What is the most preferred image format used for logos in the Wikimedia database?',
+    correct_answer: '.svg',
+    incorrect_answers: ['.png', '.jpeg', '.gif'],
   },
   {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
-    question: "In web design, what does CSS stand for?",
-    correct_answer: "Cascading Style Sheet",
+    category: 'Science: Computers',
+    type: 'multiple',
+    difficulty: 'easy',
+    question: 'In web design, what does CSS stand for?',
+    correct_answer: 'Cascading Style Sheet',
     incorrect_answers: [
-      "Counter Strike: Source",
-      "Corrective Style Sheet",
-      "Computer Style Sheet",
+      'Counter Strike: Source',
+      'Corrective Style Sheet',
+      'Computer Style Sheet',
     ],
   },
   {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
+    category: 'Science: Computers',
+    type: 'multiple',
+    difficulty: 'easy',
     question:
-      "What is the code name for the mobile operating system Android 7.0?",
-    correct_answer: "Nougat",
-    incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"],
+      'What is the code name for the mobile operating system Android 7.0?',
+    correct_answer: 'Nougat',
+    incorrect_answers: ['Ice Cream Sandwich', 'Jelly Bean', 'Marshmallow'],
   },
   {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
-    question: "On Twitter, what is the character limit for a Tweet?",
-    correct_answer: "140",
-    incorrect_answers: ["120", "160", "100"],
+    category: 'Science: Computers',
+    type: 'multiple',
+    difficulty: 'easy',
+    question: 'On Twitter, what is the character limit for a Tweet?',
+    correct_answer: '140',
+    incorrect_answers: ['120', '160', '100'],
   },
   {
-    category: "Science: Computers",
-    type: "boolean",
-    difficulty: "easy",
-    question: "Linux was first created as an alternative to Windows XP.",
-    correct_answer: "False",
-    incorrect_answers: ["True"],
+    category: 'Science: Computers',
+    type: 'boolean',
+    difficulty: 'easy',
+    question: 'Linux was first created as an alternative to Windows XP.',
+    correct_answer: 'False',
+    incorrect_answers: ['True'],
   },
   {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
+    category: 'Science: Computers',
+    type: 'multiple',
+    difficulty: 'easy',
     question:
-      "Which programming language shares its name with an island in Indonesia?",
-    correct_answer: "Java",
-    incorrect_answers: ["Python", "C", "Jakarta"],
+      'Which programming language shares its name with an island in Indonesia?',
+    correct_answer: 'Java',
+    incorrect_answers: ['Python', 'C', 'Jakarta'],
   },
-];
+]
 
 // GENERAZIONE TIMER
 
-let timer;
+let timer
 
 const countdown = function (n) {
-  let timeLeft = n;
+  let timeLeft = n
   timer = setInterval(function () {
     if (timeLeft <= 0) {
-      clearInterval(timer);
+      clearInterval(timer)
     } else {
-      document.getElementById("seconds").innerHTML = timeLeft;
+      document.getElementById('seconds').innerHTML = timeLeft
     }
-    timeLeft -= 1;
-  }, 1000);
-};
+    timeLeft -= 1
+  }, 1000)
+}
 
-countdown(60);
+countdown(60)
 
 // SEZIONE SCORE E SCOPE DEI VARI ELEMENTI
 
-const quizSection = document.getElementById("question-section");
-const quizElement = document.getElementById("question");
-const buttonsContainer = document.getElementById("buttonsContainer");
-const questionCounter = document.getElementById("question-counter");
+const quizSection = document.getElementById('question-section')
+const quizElement = document.getElementById('question')
+const buttonsContainer = document.getElementById('buttonsContainer')
+const questionCounter = document.getElementById('question-counter')
 
-let currentIndex = 0;
-let score = 0;
+let currentIndex = 0
+let score = 0
 
-const savedScore = localStorage.getItem("score");
+const savedScore = localStorage.getItem('score')
 if (savedScore !== null) {
-  score = parseInt(savedScore); //SI REGISTRA LO SCORE IN LOCALE CON GETITEM
+  score = parseInt(savedScore) //SI REGISTRA LO SCORE IN LOCALE CON GETITEM
 }
 
 // CREAZIONE DOMANDE E ELEMENTI BUTTON
 
 const getQuestion = function () {
-  const actualQuestion = questions[currentIndex];
-  quizElement.textContent = actualQuestion.question;
-  buttonsContainer.innerHTML = "";
+  const actualQuestion = questions[currentIndex]
+  quizElement.textContent = actualQuestion.question
+  buttonsContainer.innerHTML = ''
 
-  if (actualQuestion.type === "multiple") {
-    actualQuestion.incorrect_answers.push(actualQuestion.correct_answer);
+  if (actualQuestion.type === 'multiple') {
+    actualQuestion.incorrect_answers.push(actualQuestion.correct_answer)
     actualQuestion.incorrect_answers.forEach((opt) => {
-      const buttons = document.createElement("button");
-      buttons.textContent = opt;
-      buttons.addEventListener("click", () => answerChecking(opt));
-      buttonsContainer.appendChild(buttons);
-    });
-  } else if (actualQuestion.type === "boolean") {
-    const buttonTrue = document.createElement("button");
-    buttonTrue.textContent = "True";
-    buttonTrue.addEventListener("click", () => answerChecking("True"));
-    buttonsContainer.appendChild(buttonTrue);
+      const buttons = document.createElement('button')
+      buttons.textContent = opt
+      buttons.addEventListener('click', () => answerChecking(opt))
+      buttonsContainer.appendChild(buttons)
+    })
+  } else if (actualQuestion.type === 'boolean') {
+    const buttonTrue = document.createElement('button')
+    buttonTrue.textContent = 'True'
+    buttonTrue.addEventListener('click', () => answerChecking('True'))
+    buttonsContainer.appendChild(buttonTrue)
 
-    const buttonFalse = document.createElement("button");
-    buttonFalse.textContent = "False";
-    buttonFalse.addEventListener("click", () => answerChecking("False"));
-    buttonsContainer.appendChild(buttonFalse);
+    const buttonFalse = document.createElement('button')
+    buttonFalse.textContent = 'False'
+    buttonFalse.addEventListener('click', () => answerChecking('False'))
+    buttonsContainer.appendChild(buttonFalse)
   }
-};
+}
 
 // CREAZIONE COUNTER DELLE DOMANDE CHE DINAMICAMENTE CAMBIA IN BASE ALL'INDICE DELLA DOMANDA
 
-const counter = document.getElementById("question-counter");
-counter.textContent = 1;
+const counter = document.getElementById('question-counter')
+counter.textContent = 1
 
 // FUNZIONE CHE FA UN CHECK DI TRUTHY OR FALSY SULLA DOMANDA
 
 const answerChecking = function (chosenOption) {
-  const actualQuestion = questions[currentIndex];
+  const actualQuestion = questions[currentIndex]
   if (chosenOption === actualQuestion.correct_answer) {
-    score++;
+    score++
   }
-  currentIndex++;
-  counter.textContent = currentIndex + 1;
+  currentIndex++
+  counter.textContent = currentIndex + 1
   if (currentIndex < questions.length) {
-    getQuestion();
-    startAnimation(); // DOPO OGNI DOMANDA RIPARE TIL TIMER E L'ANIMAZIONE DEL TIMER
-    clearInterval(timer);
-    countdown(60);
+    getQuestion()
+    startAnimation() // DOPO OGNI DOMANDA RIPARE TIL TIMER E L'ANIMAZIONE DEL TIMER
+    clearInterval(timer)
+    countdown(60)
   } else {
-    showResult();
+    showResult()
   }
-};
+}
 
-getQuestion();
+getQuestion()
 
 //SI SALVA LO SCORE IN LOCALE CON SET ITEM
 
 const showResult = function () {
-  window.open("results.html", "_self");
-  localStorage.setItem("score", score);
-};
+  window.open('results.html', '_self')
+  localStorage.setItem('score', score)
+}
 
 //FUNZIONE CHE RIATTIVA ANIMAZIONE DEL TIIMER
 const startAnimation = function () {
-  const animationCircle = document.getElementById("animationCircle");
-  animationCircle.beginElement();
-};
+  const animationCircle = document.getElementById('animationCircle')
+  animationCircle.beginElement()
+}
