ba5ec57dcc9df2a3d9a88113b973648280bf536b
Pierpaolo Colasante <colasantepierpaolo@gmail.com>
2023-09-21 10:27:03 +0200

    aggiornamento feedback

--- script/feedback.js
+++ script/feedback.js
@@ -1,41 +1,33 @@
 // DEFINIZIONE DELLA FUNZIONE CHE ACCENDE LE STELLINE NELLA PAG.FEEDBACK
 
-const accendi = document.querySelectorAll(".star");
-let selectedStars = 0;
+const accendi = document.querySelectorAll('.star')
+let selectedStars = 0
 
 for (let i = 0; i < accendi.length; i++) {
-  accendi[i].addEventListener("click", function () {
-    selectedStars = i + 1;
+  accendi[i].addEventListener('click', function () {
+    selectedStars = i + 1
 
     for (let s = 0; s < accendi.length; s++) {
       if (s <= i) {
-        accendi[s].style.mixBlendMode = "normal";
+        accendi[s].style.mixBlendMode = 'normal'
       } else {
-        accendi[s].style.mixBlendMode = "darken";
+        accendi[s].style.mixBlendMode = 'darken'
       }
     }
-  });
+  })
 }
-
-// AGGIUNTO PREVENT DEFAULT AL FORM
-
-const form = document.querySelector("#inviaForm");
-
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  const report = form.querySelector(".box");
-  if (report.value.trim() === "") {
-    // trim() verifica che non è una stringa vuota
-    alert("Devi riempire il campo di testo");
-  } else {
-    form.submit();
-  }
-});
 
 // AGGIUNTO LINK ESTERNO AL BOTTONE FINALE
 
-const formButton = document.getElementById("form-button");
+const form = document.querySelector('#inviaForm')
+const formButton = document.getElementById('form-button')
 
-formButton.addEventListener("click", function () {
-  window.location.href = "https://www.epicode.com";
-});
+formButton.addEventListener('click', function () {
+  const report = form.querySelector('.box')
+  if (report.value.trim() === '') {
+    // trim() verifica che non è una stringa vuota
+    alert('Devi riempire il campo di testo')
+  } else {
+    window.location.href = 'https://www.epicode.com/'
+  }
+})
